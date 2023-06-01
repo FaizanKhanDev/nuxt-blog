fcd027ac7c595be540ef0b640b01cbd6ec6288ae
FaizanKhanDev <faizan480khan@gmail.com>
2023-06-01 17:19:24 +0500

    bootstrap is Setuped

---
+++ components/navbar/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-btn icon id="mode-switcher" @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+        <v-icon :color="($vuetify.theme.dark) ? 'primary' : 'primary lighten-4'">
+            {{ ($vuetify.theme.dark) ? 'mdi-weather-night' : 'mdi-weather-sunny' }}
+        </v-icon>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    computed: {
+        isDark() {
+            return (this.$vuetify.theme.dark)
+                ? 'dark'
+                : 'light'
+        }
+    },
+}
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: "~/plugins/bootstrap.js", ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -38,15 +39,16 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    ['bootstrap-vue/nuxt'],
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      light: true,
       themes: {
-        dark: {
+        light: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
