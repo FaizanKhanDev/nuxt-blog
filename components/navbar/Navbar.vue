f97d48323f909b047f31508fa571536afa43f03b
FaizanKhanDev <faizan480khan@gmail.com>
2023-06-02 15:27:35 +0500

    Fixing navbar

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -1,9 +1,42 @@
 <template>
-    <v-btn icon id="mode-switcher" @click="$vuetify.theme.dark = !$vuetify.theme.dark">
-        <v-icon :color="($vuetify.theme.dark) ? 'primary' : 'primary lighten-4'">
-            {{ ($vuetify.theme.dark) ? 'mdi-weather-night' : 'mdi-weather-sunny' }}
-        </v-icon>
-    </v-btn>
+    <div>
+        <div class="my-header" dark elevation="0" color="#000">
+            <v-img :src="require('~/assets/images/navlogo.webp')" alt="" max-height="50" max-width="50"></v-img>
+            <b-col cols="12" md="4" class="search-box">
+                <v-text-field outlined dense dark placeholder="Search" append-icon="mdi-magnify"></v-text-field>
+                <v-btn icon id="mode-switcher" @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+                    <v-icon :color="isDark ? '#fff' : '#fff'">
+                        {{ isDark ? 'mdi-weather-night' : 'mdi-weather-sunny' }}
+                    </v-icon>
+                </v-btn>
+            </b-col>
+
+
+
+        </div>
+
+        <v-app-bar class="nav" dense dark elevation="0" color="#913fe2">
+            <ul>
+
+                <li>
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+
+                <li>
+                    <nuxt-link to="/">Bookmarks</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/">Comics</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/">Recruitment</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/">Language</nuxt-link>
+                </li>
+            </ul>
+        </v-app-bar>
+    </div>
 </template>
 
 <script>
@@ -11,12 +44,60 @@ export default {
     name: "Navbar",
     computed: {
         isDark() {
-            return (this.$vuetify.theme.dark)
-                ? 'dark'
-                : 'light'
-        }
+            return this.$vuetify.theme.dark;
+        },
     },
-}
+};
 </script>
 
-<style></style>
+<style scoped>
+.my-header {
+    display: flex;
+    align-items: center;
+    height: 60px;
+    background: var(--textBlack);
+    justify-content: space-between;
+    padding: 0 7rem;
+}
+
+.search-box {
+    height: 60px;
+    display: flex;
+}
+
+#mode-switcher {
+    position: relative;
+    left: 2rem;
+}
+
+/* .nav {
+    padding: 0 7rem;
+} */
+
+.nav ul {
+    padding: 0 2rem;
+    display: flex;
+    list-style-type: none;
+    align-items: center;
+    margin: 0;
+}
+
+
+
+.nav ul li {
+    margin-left: 50px;
+    color: var(--textWhite);
+}
+
+.nav ul li a {
+    padding: 15px 15px;
+    color: var(--textWhite);
+    font-size: 0.9rem;
+    text-decoration: none;
+    font-weight: 400;
+}
+
+.nav ul li a:hover {
+    background-color: var(--secondayPurple);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,35 @@
 <template>
   <div>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field dense label="Regular"></v-text-field>
+          </v-col>
 
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field label="Filled" filled dense></v-text-field>
+          </v-col>
 
-    <button class="btn btn-danger"> Faizan Khan</button>
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field label="Filled" placeholder="Dense & Rounded" filled rounded dense></v-text-field>
+          </v-col>
+
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field label="Solo" solo dense></v-text-field>
+          </v-col>
+
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field label="Outlined" outlined dense></v-text-field>
+          </v-col>
+
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field label="Outlined" placeholder="Placeholder" outlined dense></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+
 
 
   </div>
@@ -15,6 +42,11 @@ export default {
     return {
       switch1: true,
     }
+  },
+  computed: {
+    isDark() {
+      return this.$vuetify.theme.dark;
+    },
   },
 
 }
